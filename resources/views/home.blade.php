a2350e3987e17434ec9758c77f858727e0b28bf3
Itamar Junior <cdajuniorf@gmail.com>
2025-01-28 17:36:27 -0300

    feat: @if, @elseif, @else, @endif, @switch, @case, @default, @endswitch

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.main_layout')
 @section('content')
 
-<p class="display-1 text-center">Hello World</p>
+{{-- switch --}}
+@switch($value)
+@case(100)
+<h1>Valor 100</h1>
+@break
+@case(200)
+<h1>Valor 200</h1>
+@break
+@case(300)
+<h1>Valor 300</h1>
+@break
+@default
+<h1>Outro valor</h1>
+@endswitch
 
-{{-- comentário de blade --}}
-<p>{{ $name }}</p>
 @endsection
