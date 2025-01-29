5953c8ce6a11a75b625e0c5da2ecc080a15a9832
Itamar Junior <cdajuniorf@gmail.com>
2025-01-28 21:43:46 -0300

    feat: @Production, @Env, @Error, @Enderror

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,44 +1,38 @@
 @extends('layouts.main_layout')
 @section('content')
 
-<div class="container mt-5">
-  <div class="row justify-content-center">
-    <div class="col-6">
+@production
+<p>Estou em ambiente de produção.</p>
+@else
+<p>Não estou em produção.</p>
+@endproduction
 
-      <form action="{{ route("submit") }}" method="POST">
+@env(['local', 'development'])
+<p>Estou no ambiente {{ env('APP_ENV') }}</p>
+@endenv
 
-        @csrf
+{{-- formulário --}}
+<form action="{{ route("submit") }}" method="post">
 
-        <div class="mb-3">
-          <label class="form-label">Username:</label>
-          <input type="text" name="username" class="form-control">
-        </div>
+  @csrf
 
-        <div class="mb-3">
-          <label class="form-label">Password:</label>
-          <input type="password" name="password" class="form-control">
-        </div>
-
-        <div class="mb-3">
-          <button type="submit" class="btn btn-primary">Login</button>
-        </div>
-
-      </form>
-    </div>
+  <div>
+    <input type="text" name="name">
+    @error('name')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
   </div>
-</div>
 
-{{-- executar PHP dentro de uma view --}}
-@php
+  <div>
+    <input type="text" name="country">
+    @error('country')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+  </div>
 
-$valor = 100;
-$valor1 = '<span class="text-warning">' . $valor . '</span>';
-$nome = "itamar junior";
-@endphp
+  <button type="submit">Enviar</button>
 
-<h3> {{ $valor }}</h3>
-<h3> {!! $valor1 !!}</h3>
-<h3> {{ $valor * 1000  }}</h3>
+</form>
 
-<h3> $nome tem <span class="text-info">{{ strlen($nome) }}</span> caracteres</h3>
+
 @endsection
